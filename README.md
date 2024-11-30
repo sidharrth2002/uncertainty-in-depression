e08b0fd7e77a09a2268f9306b003f8b4dd114ce1
Sidharrth Nagappan <sidharrth2002@gmail.com>
2024-11-30 15:35:06 +0000

    update models

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # uncertainty-in-depression
 
 The beginning of something great.
+
+This is how the model looks like:
+
+1. Visual Modality - Processed Using Conv2D + BiLSTM
+2. Audio Modality - Processed Using Conv1D + BiLSTM
+3. Text Modality - Processed Using Conv1D + BiLSTM
--- coldfusion/model.py
+++ coldfusion/model.py
@@ -14,9 +14,13 @@ class LatentGRU(nn.Module):
     def forward(self, x, lengths=None):
         # x: (batch_size, seq_len, input_size)
         if lengths is not None:
-            packed_input = nn.utils.rnn.pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+            packed_input = nn.utils.rnn.pack_padded_sequence(
+                x, lengths, batch_first=True, enforce_sorted=False
+            )
             packed_output, _ = self.gru(packed_input)
-            output, _ = nn.utils.rnn.pad_packed_sequence(packed_output, batch_first=True)
+            output, _ = nn.utils.rnn.pad_packed_sequence(
+                packed_output, batch_first=True
+            )
         else:
             output, _ = self.gru(x)
         # Use the last hidden state
@@ -24,19 +28,97 @@ class LatentGRU(nn.Module):
         mu = self.fc_mu(h_n)
         logvar = self.fc_logvar(h_n)
         var = torch.exp(logvar)
-        return mu, var    
+        return mu, var
+
 
 class COLDModel(nn.Module):
-    def __init__(self, config: Config):
+    def __init__(self):
         super(COLDModel, self).__init__()
 
-        self.audio_extractor = AudioFeatureExtractor()
-        self.text_extractor = TextFeatureExtractor(config.vocab_size, config.embedding_dim, config.hidden_size)
+        config = {
+            "visual_net": {
+                "input_dim": 3,
+                "output_dim": 256,
+                "conv_hidden": 256,
+                "lstm_hidden": 256,
+                "num_layers": 4,
+                "activation": "relu",
+                "norm": "bn",
+                "dropout": 0.6,
+            },
+            "audio_net": {
+                "input_dim": 80,
+                "output_dim": 256,
+                "conv_hidden": 256,
+                "lstm_hidden": 256,
+                "num_layers": 4,
+                "activation": "relu",
+                "norm": "bn",
+                "dropout": 0.6,
+            },
+            "text_net": {
+                "input_dim": 512,
+                "output_dim": 256,
+                "conv_hidden": 256,
+                "lstm_hidden": 256,
+                "num_layers": 2,
+                "activation": "relu",
+                "norm": "bn",
+                "dropout": 0.6,
+            },
+        }
 
-        self.audio_temporal = LatentGRU()
-        self.text_temporal = LatentGRU()
+        self.audio_extractor = ConvLSTM_Audio(
+            input_dim=config["audio_net"]["input_dim"],
+            output_dim=config["audio_net"]["output_dim"],
+            conv_hidden=config["audio_net"]["conv_hidden"],
+            lstm_hidden=config["audio_net"]["lstm_hidden"],
+            num_layers=config["audio_net"]["num_layers"],
+            activation=config["audio_net"]["activation"],
+            norm=config["audio_net"]["norm"],
+            dropout=config["audio_net"]["dropout"],
+        )
 
+        # self.text_extractor = ConvLSTM_Text(
+        #     input_dim=config["text_net"]["input_dim"],
+        #     output_dim=config["text_net"]["output_dim"],
+        #     conv_hidden=config["text_net"]["conv_hidden"],
+        #     lstm_hidden=config["text_net"]["lstm_hidden"],
+        #     num_layers=config["text_net"]["num_layers"],
+        #     activation=config["text_net"]["activation"],
+        #     norm=config["text_net"]["norm"],
+        #     dropout=config["text_net"]["dropout"],
+        # )
 
+        self.visual_extractor = ConvLSTM_Visual(
+            input_dim=config["visual_net"]["input_dim"],
+            output_dim=config["visual_net"]["output_dim"],
+            conv_hidden=config["visual_net"]["conv_hidden"],
+            lstm_hidden=config["visual_net"]["lstm_hidden"],
+            num_layers=config["visual_net"]["num_layers"],
+            activation=config["visual_net"]["activation"],
+            norm=config["visual_net"]["norm"],
+            dropout=config["visual_net"]["dropout"],
+        )
+
+        # GRU used to estimate the mean and variance of the latent space
+        self.audio_temporal = LatentGRU(
+            input_size=config["audio_net"]["output_dim"],
+            hidden_size=256,
+            output_size=128
+        )
+        # self.text_temporal = LatentGRU(
+        #     input_size=config["text_net"]["output_dim"],
+        #     hidden_size=256,
+        #     output_size=128
+        # )
+        self.visual_temporal = LatentGRU(
+            input_size=config["visual_net"]["output_dim"],
+            hidden_size=256,
+            output_size=128
+        )
+
+    
     def compute_fusion_weights(self, variance_modalities):
         """
         Compute fusion weights based on the variance of each modality.
@@ -54,20 +136,30 @@ class COLDModel(nn.Module):
 
     def forward(self, audio_features, text_features):
         audio_features = self.audio_extractor(audio_features)
-        
         mean_audio, variance_audio = self.audio_temporal(audio_features)
 
-        text_features = self.text_extractor(text_features)
+        # text_features = self.text_extractor(text_features)
+        # mean_text, variance_text = self.text_temporal(text_features)
 
-        mean_text, variance_text = self.text_temporal(text_features)
+        visual_features = self.visual_extractor(visual_features)
+        mean_visual, variance_visual = self.visual_temporal(visual_features)
 
-        mean_modalities = [mean_audio, mean_text]
-        variance_modalities = [variance_audio, variance_text]
+        mean_modalities = [mean_audio, mean_visual]
+        variance_modalities = [variance_audio, variance_visual]
 
         weights = self.compute_fusion_weights(variance_modalities)
 
-        fused_mean = sum(weight * mean for weight, mean in zip(weights, mean_modalities))
+        fused_mean = sum(
+            weight * mean for weight, mean in zip(weights, mean_modalities)
+        )
 
         y_pred = self.output_layer(fused_mean)
 
         return y_pred, mean_modalities, variance_modalities
+
+if __name__ == "__main__":
+    model = COLDModel()
+    print(model)
+    # count number of parameters
+    num_params = sum(p.numel() for p in model.parameters())
+    print(f"Number of parameters: {num_params}")
